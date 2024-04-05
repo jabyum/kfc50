985a039c0564534860657d8a73e49155c1a21d11
jabyum <jabyum@github.com>
2024-04-05 19:15:52 +0500

    database

--- bot.py
+++ bot.py
@@ -1,18 +1,26 @@
 import telebot
 import buttons as bt
 from geopy.geocoders import Nominatim
-bot = telebot.TeleBot("")
+import database as db
+bot = telebot.TeleBot("6571068957:AAEwA9k4Jule6IiL2L6_aF62zu8c_tSbivc")
 # объект для расшифровки координат
 geolocator = Nominatim(user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15")
-
+# db.add_product(pr_name= "Чизбургер2", pr_quantity=0, pr_price=20000.0, pr_des="лучший", pr_photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzolUu5EuGCYGg--0U4LV8vuQ0w1nHKxvMjiPgIqxCSA&s")
+# print(db.get_all_products())
+# print(db.get_pr_id_name())
 @bot.message_handler(commands=["start"])
 def start(message):
     user_id = message.from_user.id
-    bot.send_message(user_id, "Здравствуйте! "
-                              "Добро пожаловаться в бот от KFC!\n"
-                              "Пройдите короткую регистрацию.\n\n"
-                              "Введите своё имя:")
-    bot.register_next_step_handler(message, get_name)
+    checker = db.check_user(user_id)
+    if checker == False:
+        bot.send_message(user_id, "Здравствуйте! "
+                                  "Добро пожаловаться в бот от KFC!\n"
+                                  "Пройдите короткую регистрацию.\n\n"
+                                  "Введите своё имя:")
+        bot.register_next_step_handler(message, get_name)
+    elif checker == True:
+        bot.send_message(user_id, "Выберите действие:", reply_markup=bt.main_menu_kb())
+
 def get_name(message):
     user_id = message.from_user.id
     name = message.text
@@ -28,6 +36,7 @@ def get_phone_number(message, name):
         print(phone_number)
         bot.send_message(user_id, "Номер принят. Регистрация завершена."
                                   "Выберите действие:", reply_markup=bt.main_menu_kb())
+        db.add_user(user_id=user_id, name=name, phone_number=phone_number)
     # возвращаем пользователя в эту же функцию, если он отправил не через кнопку
     else:
         bot.send_message(user_id, "Отправьте свои контакты через кнопку")
---
+++ database.py
@@ -0,0 +1,103 @@
+import sqlite3
+from datetime import datetime
+connection = sqlite3.connect("kfc.db")
+sql = connection.cursor()
+sql.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER, "
+            "name TEXT, phone_number TEXT, reg_date DATETIME);")
+sql.execute("CREATE TABLE IF NOT EXISTS products (pr_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            "pr_name TEXT,  pr_price REAL, pr_quantity INTEGER, pr_des TEXT,"
+            "pr_photo TEXT, reg_date DATETIME);")
+sql.execute("CREATE TABLE IF NOT EXISTS cart (user_id INTEGER, pr_id INTEGER, "
+            "pr_name TEXT, pr_count INTEGER, total_price REAL);")
+connection.commit()
+
+def add_user(user_id, name, phone_number):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    sql.execute("INSERT INTO users (user_id, name, phone_number,"
+                "reg_date) VALUES (?,?,?,?);", (user_id, name,
+                                                phone_number, datetime.now()))
+    connection.commit()
+def check_user(user_id):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    # пробуем вытащить пользователя из базы данных по его айди
+    checker = sql.execute("SELECT user_id FROM users "
+                          "WHERE user_id=?;", (user_id, )).fetchone()
+    # если мы его нашли в базе данных
+    if checker:
+        return True
+    # если не нашли в бд
+    return False
+def get_all_users():
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    users = sql.execute("SELECT * FROM users;").fetchall()
+    return users
+
+def add_product(pr_name, pr_quantity, pr_price, pr_des, pr_photo):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    sql.execute("INSERT INTO products (pr_name, pr_price, pr_quantity, "
+                "pr_des, pr_photo, reg_date) VALUES (?,?,?,?,?,?);",
+                (pr_name, pr_price, pr_quantity, pr_des, pr_photo, datetime.now()))
+    connection.commit()
+def get_all_products():
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    all_products = sql.execute("SELECT * FROM products;").fetchall()
+    return all_products
+def get_exact_produdct(pr_id):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    exact_product = sql.execute("SELECT pr_name, pr_price, pr_des, pr_photo FROM products "
+                                "WHERE pr_id=?;", (pr_id, )).fetchone()
+    return exact_product
+def get_pr_id_name():
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    all_products = sql.execute("SELECT pr_id, pr_name, pr_quantity FROM products;").fetchall()
+    actual_products = [(i[0], i[1]) for i in all_products if i[2]>0]
+    return actual_products
+def get_all_id():
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    all_id = sql.execute("SELECT pr_id FROM products;").fetchall()
+    actual_id = [i[0] for i in all_id]
+    return actual_id
+def delete_products():
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    sql.execute("DELETE FROM products;")
+    connection.commit()
+    return get_all_products()
+def delete_exact_products(pr_id):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    sql.execute("DELETE FROM products WHERE pr_id=?;", (pr_id, ))
+    connection.commit()
+def change_quantity(pr_id, new_quantity):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    sql.execute("UPDATE products SET pr_quantity=? WHERE pr_id=?;",
+                (new_quantity, pr_id))
+    connection.commit()
+# корзина
+def add_to_cart(user_id, pr_id, pr_name, pr_count, pr_price):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+    total_price = pr_count * pr_price
+    sql.execute("INSERT INTO cart (user_id, pr_id, pr_name, pr_count, total_price) "
+                "VALUES (?,?,?,?,?);", (user_id, pr_id, pr_name,
+                                        pr_count, total_price))
+    connection.commit()
+
+# удаление продукта из корзины
+def delete_exact_pr_from_cart(user_id, pr_id):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
+
+# получение корзины пользователя (fetchall())
+def get_user_cart(user_id):
+    connection = sqlite3.connect("kfc.db")
+    sql = connection.cursor()
